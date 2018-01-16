955ec0e4e347fcc1edade2d7606ea9d6be82902d
CY-nb <31556438+c3p0cy@users.noreply.github.com>
2018-01-16 08:08:42 +0800

    init

---
+++ src/main/java/tw/c3p0cy/practice/springbootmocktesting/SpringBootMockTestingApplication.java
@@ -0,0 +1,12 @@
+package tw.c3p0cy.practice.springbootmocktesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMockTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMockTestingApplication.class, args);
+	}
+}
---
+++ src/test/java/tw/c3p0cy/practice/springbootmocktesting/SpringBootMockTestingApplicationTests.java
@@ -0,0 +1,16 @@
+package tw.c3p0cy.practice.springbootmocktesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMockTestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
