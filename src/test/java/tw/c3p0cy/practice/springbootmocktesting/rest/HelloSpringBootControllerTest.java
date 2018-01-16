7e3c2ca94f94362f1f82406e0ccbf0edddbb7105
CY-nb <31556438+c3p0cy@users.noreply.github.com>
2018-01-16 11:53:41 +0800

    Use MockMvc to test HelloSpringBootController

---
+++ src/test/java/tw/c3p0cy/practice/springbootmocktesting/rest/HelloSpringBootControllerTest.java
@@ -0,0 +1,39 @@
+package tw.c3p0cy.practice.springbootmocktesting.rest;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {MockServletContext.class})
+public class HelloSpringBootControllerTest {
+
+  private MockMvc mockMvc;
+
+  @Before
+  public void setUp() {
+    mockMvc = MockMvcBuilders.standaloneSetup(new HelloSpringBootController()).build();
+  }
+
+  @Test
+  public void hello() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(content().string(equalTo("Hello Spring Boot!")))
+        .andDo(MockMvcResultHandlers.print())
+        .andReturn();
+  }
+}
